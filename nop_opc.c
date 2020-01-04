794ef45363f0b3ebe12c3f9f90864a83c1173ad0
arcewp15 <970@holbertonschool.com>
2020-01-04 16:49:58 -0500

    [add] adding the nop implementation

---
+++ nop_opc.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * nop_opcode - Doesn't do anything
+ * @stack: pointer to the stack
+ * @n_line: current line number
+ *
+ * Return: void
+ */
+void nop_opcode(stack_t **stack, unsigned int n_line)
+{
+	(void)stack;
+	(void)n_line;
+}
